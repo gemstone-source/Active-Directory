72c182ded9b838256942b6ad980987e581f7720b
gemstone-source <sophiajulius5@gmail.com>
2022-07-03 21:10:20 +0300

    Add some powershell script for random user generator

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -6,6 +6,14 @@ function CreateADGroup(){
     New-ADGroup -name $name -GroupScope Global
 
 }
+
+function RemoveADGroup(){
+    param([Parameter(Mandatory=$true)] $groupObject)
+    
+    $name = $groupObject.name
+    Remove-ADGroup -Identity $name -Confirm:$False
+}
+
 function CreateADUser(){
     param([Parameter(Mandatory=$true)] $userObject)
 
@@ -32,11 +40,18 @@ function CreateADUser(){
         catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
         {
             Write-Warning "User $name NOT added to group $group_name because it does not exist"
-        }
-        
-        
+        } 
     }
 }
+
+function WeakenPasswordPolicy(){
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
+
+WeakenPasswordPolicy
 
 $json  = ( Get-Content $JSONFile | ConvertFrom-JSON )
 
